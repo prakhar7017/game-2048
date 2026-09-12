9b03951ba43f1621bac37a8ad922cc14cba708f3
Prakhar Maheshwari <prakharmaheshwari96@gmail.com>
2026-09-12 20:01:51 +0530

    added tile component

---
+++ src/features/game2048/components/tile.tsx
@@ -0,0 +1,21 @@
+type TileProp = {
+  value: number;
+  row: number;
+  col: number;
+};
+
+export function Tile({ value, row, col }: TileProp) {
+  if (value == 0) return null;
+
+  return (
+    <div
+      className={`tile tile-{value}`}
+      style={{
+        gridColumn: col + 1,
+        gridRow: row + 1,
+      }}
+    >
+      {value}
+    </div>
+  );
+}
